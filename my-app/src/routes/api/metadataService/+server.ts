54d677ea638a36e76c8c5842ec11196e9996934c
Luca SK <sarifluca@gmail.com>
2025-03-02 10:40:23 +0000

    metadat

--- my-app/src/routes/api/metadataService/+server.ts
+++ my-app/src/routes/api/metadataService/+server.ts
@@ -6,7 +6,10 @@ export const POST = async ({cookies, request}) => {
     const data = await request.json();
     const { userAddress, token, productName } = data;
 
-    const metadata = JSON.stringify({ token: token, history: [{[userAddress]: Date.now().toString()}], productName });
+    const oldMetadata = JSON.parse(fs.readFileSync(`metadata/${token}.json`, "utf8"));
+    console.log("oldMetadata: ", oldMetadata, oldMetadata.history);
+    
+    const metadata = JSON.stringify({ token: token, history: oldMetadata.history.push({[userAddress]: Date.now().toString()}), productName });
     fs.writeFileSync(`metadata/${token}.json`, metadata, "utf8");
 
     await storage.bucket('wrangler-5a833.appspot.com').upload(`metadata/${token}.json`);
